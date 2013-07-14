2469226e3540ace0f66e1983350ea725a43430d2
hrk-ys <hiroki.yoshifuji@gmail.com>
2013-07-15 02:06:15 +0900

    first commit

---
+++ Sample1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample1
+//
+//  Created by Hiroki Yoshifuji on 13/07/15.
+//  Copyright (c) 2013年 Hiroki Yoshifuji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
